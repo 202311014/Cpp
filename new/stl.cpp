3abfa3b897f4294761debe3da54485d178f24330
202311014 <202311014@daiict.ac.in>
2023-09-25 12:15:19 +0530

    new

--- new/stl.cpp
+++ new/stl.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 int main() {
-    
+    array<int , 5> a = { 1,2,3,4,5};
+    int size = a.size();
+
+    for (int i =0; i<size ; i++){
+        cout << a[i] << endl;
+    }
 }
