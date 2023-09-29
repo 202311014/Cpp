4fac6ad9fa07ec49efed7aa71369c21f4d123f57
202311014 <202311014@daiict.ac.in>
2023-09-29 13:22:22 +0530

    New

--- new/stl.cpp
+++ new/stl.cpp
@@ -5,7 +5,11 @@ int main() {
     array<int , 5> a = { 1,2,3,4,5};
     int size = a.size();
 
-    for (int i =0; i<size ; i++){
+    for (int i =0; i<size ; i++)
+    {
         cout << a[i] << endl;
     }
+    cout << "Empty or not " << a.empty() << endl;
+    
+    cout << "Element at 1 is " << a.at(1) ;
 }
